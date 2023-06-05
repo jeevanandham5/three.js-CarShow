000b330dca659ea335a4c2cfcbf78e22395bfc8a
jeevanandham5 <jeevajeevanandham30@gmail.com>
2023-06-05 15:12:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "react-three-fiber";
+import { Suspense, useEffect, useState } from "react";
+import "./App.css";
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import Ground from "./ground";
+import Car from "./car";
+import Rings from "./Rings";
 
-function App() {
+//import {Bloom, ChromaticAberration, EffectComposer,} from "@react-three/postprocessing";
+//import { BlendFunction } from "postprocessing";
+
+function Carshow() {
+  const [audio] = useState(new Audio("thebox.mp3"));
+
+  const handleInteract = () => {
+    audio.play();
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleInteract);
+    return () => {
+      document.removeEventListener("click", handleInteract);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <color args={[0, 0, 0]} attach="background" />
+
+      <CubeCamera frames={Infinity} resolution={256}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
+      <Rings />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <Ground />
+
+      {/*<EffectComposer>
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={0.1}
+          width={300}
+          height={300}
+          kernelSize={5}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.025}
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL}
+          offset={[0.0005, 0.0012]}
+        />
+      </EffectComposer>*/}
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <Carshow />
+      </Canvas>
+    </Suspense>
+  );
+}
---
+++ src/car.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useFrame, useLoader } from "react-three-fiber";
+import { Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+export default function Car() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "model/car/scene.gltf"
+  );
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((object) => {
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+  useFrame((state, delta) => {
+    let t = state.clock.getElapsedTime();
+    let group = gltf.scene.children[0].children[0].children[0];
+    group.children[0].rotation.x = t * 2;
+    group.children[2].rotation.x = t * 2;
+    group.children[4].rotation.x = t * 2;
+    group.children[6].rotation.x = t * 2;
+  });
+  return <primitive object={gltf.scene} />;
+}
